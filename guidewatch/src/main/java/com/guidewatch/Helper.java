ced7dfd43e5f06d94bbd1284251be0d58348d4cd
beardyFace <HenryAMWilliams@gmail.com>
2016-11-14 07:29:05 +1300

    Divided into watch and mobile app, with common core api.

---
+++ guidewatch/src/main/java/com/guidewatch/Helper.java
@@ -0,0 +1,11 @@
+package com.guidewatch;
+
+/**
+ * Created by Henry on 05-Aug-16.
+ */
+public abstract class Helper {
+
+    public static double round(double i, int v){
+        return (Math.round(i/v) * v);
+    }
+}
